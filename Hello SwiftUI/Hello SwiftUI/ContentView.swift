6ee0c6133009be4cb96694c311123fb43958b818
JasonHaque <zahinjason220434@gmail.com>
2020-11-04 20:44:14 +0600

    added  code

---
+++ Hello SwiftUI/Hello SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello SwiftUI
+//
+//  Created by Sanviraj Zahin Haque on 4/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI ... this is Jason !")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello SwiftUI/Hello SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello SwiftUI
+//
+//  Created by Sanviraj Zahin Haque on 4/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
